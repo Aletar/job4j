844afffd5ee490047141e6ee7d7de6cff6b1a29a
Alexander Tarasyuk <alextarasyuk@gmail.com>
2020-01-23 20:21:54 +0700

    Исправил замечание к задаче [#211589]: убрал проверку, что конечный индекс может быть больше размера массива

--- chapter_001/src/main/java/ru/job4j/array/MinDiapason.java
+++ chapter_001/src/main/java/ru/job4j/array/MinDiapason.java
@@ -3,8 +3,7 @@ package ru.job4j.array;
 public class MinDiapason {
     public static int findMin(int[] array, int start, int finish) {
         int min = array[start];
-        int end = finish < array.length - 1 ? finish : array.length - 1;
-        for (int index = start + 1; index <= end; index++) {
+        for (int index = start + 1; index <= finish; index++) {
             min = array[index] < min ? array[index] : min;
         }
         return min;
