76bfe77339506cf13745a5aea199b39d9c99eabf
Alexander Tarasyuk <alextarasyuk@gmail.com>
2020-02-19 07:35:45 +0700

    3. Перегрузить метод max для трех чисел [#211717]
    Сделал, чтобы логика сравнения была только в методе от двух аргументов

--- chapter_001/src/main/java/ru/job4j/condition/Max.java
+++ chapter_001/src/main/java/ru/job4j/condition/Max.java
@@ -7,16 +7,12 @@ public class Max {
     }
 
     public static int max(int first, int second, int third) {
-        int left = max(first, second);
-        int right = third;
-        int result = left > right ? left : right;
+        int result = max(max(first, second), third);
         return result;
     }
 
     public static int max(int first, int second, int third, int fourth) {
-        int left = max(first, second);
-        int right = max(third, fourth);
-        int result = left > right ? left : right;
+        int result = max(max(first, second), max(third, fourth));
         return result;
     }
 }
