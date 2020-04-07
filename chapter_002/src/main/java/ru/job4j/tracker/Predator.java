1bec1cc6a695f8fb0543ff061fe41e986a63bd4d
Alexander Tarasyuk <alextarasyuk@gmail.com>
2020-04-07 09:33:31 +0700

    Перегрузка конструктора [#211730] Добавил в классы Predator и Tiger конструктор по умолчанию

--- chapter_002/src/main/java/ru/job4j/tracker/Predator.java
+++ chapter_002/src/main/java/ru/job4j/tracker/Predator.java
@@ -2,7 +2,12 @@ package ru.job4j.tracker;
 
 public class Predator extends Animal {
     public Predator() {
-        super();
         System.out.println("Predator");
     }
+
+    public Predator(String name) {
+        super(name);
+        System.out.println("Predator");
+    }
+
 }
