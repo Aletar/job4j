e64ee1d3a26b3ddfd80914073765103ff8a8ea29
Alexander Tarasyuk <alextarasyuk@gmail.com>
2020-04-12 22:53:17 +0700

    5. Реализовать класс Tracker [#211740]

--- chapter_002/src/main/java/ru/job4j/tracker/Item.java
+++ chapter_002/src/main/java/ru/job4j/tracker/Item.java
@@ -1,14 +1,31 @@
 package ru.job4j.tracker;
 
+import java.util.Objects;
+
 public class Item {
+    private String id;
+    private String name;
 
     public Item() {
     }
 
     public Item(String name) {
+        this.name = name;
     }
 
-    public static void main(String[] args) {
-        Item item = new Item();
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
--- chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
+++ chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,4 +1,75 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class Tracker {
+    /**
+     * Массив для хранения заявок.
+     */
+    private final Item[] items = new Item[100];
+
+    /**
+     * Указатель ячейки для новой заявки.
+     */
+    private int position = 0;
+
+    /**
+     * Метод добавления заявки в хранилище
+     * @param item новая заявка
+     */
+    public Item add(Item item) {
+        item.setId(generateId());
+        items[position++] = item;
+        return item;
+    }
+
+    /**
+     * Метод генерирует уникальный ключ для заявки.
+     * Так как у заявки нет уникальности полей, имени и описание. Для идентификации нам нужен уникальный ключ.
+     * @return Уникальный ключ.
+     */
+    private String generateId() {
+        Random rm = new Random();
+        return String.valueOf(rm.nextLong() + System.currentTimeMillis());
+    }
+
+    /**
+     * Метод формирует копию массива без элементов null.
+     * @return массив Item.
+     */
+    public Item[] findAll() {
+        return items;
+    }
+
+    /**
+     * Поиск все элементов с именем key
+     * @param key имя элемента
+     * @return массив Item.
+     */
+    public Item[] findByName(String key) {
+        Item[] result = new Item[position];
+        int size = 0;
+        for (int index = 0; index < position; index++) {
+            if (items[index].getName().equals(key)) {
+                result[size] = items[index];
+                size++;
+            }
+        }
+        return Arrays.copyOf(result, size);
+    }
+
+    /**
+     * Поиск элемента по уникальному ключу
+     * @param id уникальный ключ
+     * @return Item.
+     */
+    public Item findById(String id) {
+        for (int index = 0; index < position; index++) {
+            if (items[index].getId().equals(id)) {
+                return items[index];
+            }
+        }
+        return null;
+    }
 }
