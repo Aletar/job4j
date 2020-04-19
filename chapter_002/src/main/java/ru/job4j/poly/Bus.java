b2baa5a4cb3c50426224fbf93eceaf55bb7ffcc3
Alexander Tarasyuk <alextarasyuk@gmail.com>
2020-04-19 22:43:29 +0700

    3. Интерфейс Input [#211695]

---
+++ chapter_002/src/main/java/ru/job4j/poly/Bus.java
@@ -0,0 +1,18 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport {
+    @Override
+    public void drive() {
+
+    }
+
+    @Override
+    public void passengers(int count) {
+
+    }
+
+    @Override
+    public float fillUp(float fuel) {
+        return 0;
+    }
+}
