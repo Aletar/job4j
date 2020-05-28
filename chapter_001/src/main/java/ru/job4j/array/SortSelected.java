9d4920c66b5b3c181b2e69ed96fc1ceb9a858e4e
Alexander Tarasyuk <alextarasyuk@gmail.com>
2020-05-28 21:24:48 +0700

    Исправил ошибку в первом проекте

--- chapter_001/src/main/java/ru/job4j/array/SortSelected.java
+++ chapter_001/src/main/java/ru/job4j/array/SortSelected.java
@@ -4,8 +4,8 @@ public class SortSelected {
     public static int[] sort(int[] data) {
         int min, index, tmp;
         for (int i = 0; i < data.length; i++) {
-            min = MinDiapason.findMin(data, i, data.length);
-            index = FindLoop.indexOf(data, min, i, data.length);
+            min = MinDiapason.findMin(data, i, data.length - 1);
+            index = FindLoop.indexOf(data, min, i, data.length - 1);
             tmp = data[i];
             data[i] = data[index];
             data[index] = tmp;
